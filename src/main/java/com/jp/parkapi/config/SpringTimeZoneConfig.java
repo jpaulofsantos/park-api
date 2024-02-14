a63db599aef5a1befae49e7f8acec0511bfa55c6
JoaoPaulo <jpaulofsantos@gmail.com>
2024-02-13 22:05:59 -0300

    add locale first configs

---
+++ src/main/java/com/jp/parkapi/config/SpringTimeZoneConfig.java
@@ -0,0 +1,15 @@
+package com.jp.parkapi.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimeZoneConfig {
+
+    @PostConstruct //apos a classe ser inicializada pelo Spring, o metodo é executado
+    public void timeZoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+}
