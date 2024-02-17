e26d90690da98aec64bcb041f9fc278258dd6d9e
JoaoPaulo <jpaulofsantos@gmail.com>
2024-02-17 10:09:54 -0300

    first config for instegration tests (pom xml dependency, sql scripts)

---
+++ src/test/resources/sql/users/users.insert.sql
@@ -0,0 +1,10 @@
+insert into USERS (id, username, password, role) values (100, "teste20@gmail.com", "123456", "ROLE_ADMIN");
+insert into USERS (id, username, password, role) values (101, "teste21@gmail.com", "123456", "ROLE_CLIENT");
+insert into USERS (id, username, password, role) values (102, "teste22@gmail.com", "123456","ROLE_ADMIN");
+insert into USERS (id, username, password, role) values (103, "teste23@gmail.com", "123456","ROLE_CLIENT");
+insert into USERS (id, username, password, role) values (104, "teste24@gmail.com", "123456","ROLE_CLIENT");
+insert into USERS (id, username, password, role) values (105, "teste25@gmail.com", "123456","ROLE_CLIENT");
+insert into USERS (id, username, password, role) values (106, "teste26@gmail.com", "123456","ROLE_CLIENT");
+insert into USERS (id, username, password, role) values (107, "teste27@gmail.com", "123456","ROLE_ADMIN");
+insert into USERS (id, username, password, role) values (108, "teste28@gmail.com", "123456","ROLE_CLIENT");
+insert into USERS (id, username, password, role) values (109, "teste29@gmail.com", "123456","ROLE_CLIENT");
